d42667fd61b9787acd185d3809c15ab3683cabe9
Saman <samangithoseinpour@gmail.com>
2023-08-19 19:01:35 +0330

    style: css config files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col justify-between items-center padding-t">
+      <h1 className="text-blue-600 text-2xl font-extrabold">Nike Store</h1>
+    </div>
+  );
 };
 
 export default App;
