10ebc577a62d2262b024491c3a4be4ed3504c83b
Saman <samangithoseinpour@gmail.com>
2023-09-04 12:01:24 +0330

    feat: implement SuperQuality section

--- src/components/SuperQuality.jsx
+++ src/components/SuperQuality.jsx
@@ -1,5 +1,45 @@
+import { Button } from './index';
+import { shoe8 } from '../assets/images';
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We provide you
+          <span className="text-coral-red"> Super</span>
+          <span className="text-coral-red"> Quality</span> Shoes
+          <br />
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Discover stylish Nike arrivals, quality comfort and innovation for
+          your active life.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+      <div className="flex flex-1 justify-center items-center">
+        <img
+          src={shoe8}
+          alt="shoe"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
+
+<p className="text-xl font-montserrat text-coral-red">
+  Our Summer Collections
+</p>;
