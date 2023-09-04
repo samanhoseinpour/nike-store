cb6889a5c04cd30650f3c6b90c8401a72652e7e6
Saman <samangithoseinpour@gmail.com>
2023-09-04 12:51:41 +0330

    feat: SpecialOffers section

--- src/components/SpecialOffers.jsx
+++ src/components/SpecialOffers.jsx
@@ -1,5 +1,45 @@
+import { offer } from '../assets/images';
+import { Button } from './index';
+import { arrowRight } from '../assets/icons';
+
 const SpecialOffers = () => {
-  return <div>SpecialOffers</div>;
+  return (
+    <section className="flex justify-center items-center max-xl:flex-col-reverse max-container gap-10">
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt="Shoe promotion"
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-col flex-1">
+        <h2 className="text-4xl font-palanquin font-bold">
+          <span className="text-coral-red">Special</span> Offers
+        </h2>
+        <p className="mt-4 info-text">
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+        <p className="mt-6 info-text">
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop now" iconURL={arrowRight} />
+          <Button
+            label="Learn more"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SpecialOffers;
